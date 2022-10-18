4f0cf57b2b2b2f03725160f4cf334606c9939996
Yiqun Ding <yiqunding@zju.edu.cn>
2022-10-18 20:59:45 +0800

    initial impl

---
+++ lua/fcitx.lua
@@ -0,0 +1,57 @@
+local uv = vim.loop
+local api = vim.api
+local fn = vim.fn
+
+local M = {}
+-- fcitx_remote_cmd can be fcitx-remote for fcitx4 or fcitx5-remote for fcitx5
+local fcitx_remote_cmd = ""
+
+local function leave_insert()
+  -- fcitx5-remote takes 0.003-0.005s to run, no need to go async
+  local fcitx_status = tonumber(vim.fn.system(fcitx_remote_cmd))
+  -- 2 means fcitx is active, 1 means inactive
+  if fcitx_status == 2 then
+    -- fcitx_activate_in_insert means whether to activate fcitx next time in INSERT mode
+    vim.b.fcitx_activate_in_insert = true
+    -- always switch to en in NORMAL mode
+    fn.system(fcitx_remote_cmd .. " -c")
+  else
+    vim.b.fcitx_activate_in_insert = false
+  end
+end
+
+local function enter_insert()
+  if vim.b.fcitx_activate_in_insert == true then
+    -- switch to cn
+    fn.system(fcitx_remote_cmd .. " -o")
+  end
+end
+
+M.setup = function()
+  -- no need to switch input method in a SSH session
+  if uv.os_getenv("SSH_TTY") ~= nil then
+    return
+  end
+
+  -- no need to switch input method if not in GUI
+  if uv.os_getenv("DISPLAY") == nil and uv.os_getenv("WAYLAND_DISPLAY") == nil then
+    return
+  end
+
+  -- defaults to fcitx5-remote
+  if fn.executable("fcitx5-remote") == 1 then
+    fcitx_remote_cmd = "fcitx5-remote"
+  elseif fn.executable("fcitx-remote") == 1 then
+    fcitx_remote_cmd = "fcitx-remote"
+  else
+    print("fcitx-remote/fcitx5-remote not in PATH, Aborting setup")
+    return
+  end
+
+  -- setup autocmds
+  local fcitx_augroup = api.nvim_create_augroup("fcitx_augroup", { clear = true })
+  api.nvim_create_autocmd("InsertEnter", { callback = enter_insert, group = fcitx_augroup })
+  api.nvim_create_autocmd("InsertLeave", { callback = leave_insert, group = fcitx_augroup })
+end
+
+return M
